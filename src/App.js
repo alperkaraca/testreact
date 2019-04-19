b3e2e9d2e06612bb19abea270ade40499c0c9919
alper <alperkaraca@gmail.com>
2019-04-20 01:18:51 +0300

    v18-router-using

--- src/App.js
+++ src/App.js
@@ -3,25 +3,39 @@ import Navbar from "./components/Navbar";
 import AddUser from "./components/AddUser";
 import Users from "./components/Users";
 // import Test from './components/Test';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import './App.css';
+
+
+const Home = () => {
+  return (
+    <h3>Home Page</h3>
+  )
+}
+
+const About = () => {
+  return (
+    <h3>About Page</h3>
+  )
+}
 
 class App extends Component {
 
   render() {
-    
+
     return (
-      <div className="container">
-        {/*<Test test = "deneme" />*/}
-        <Navbar title="User App"/>
-        {/*<Navbar/>*/}
-        <hr/>
-        <AddUser/>
-        
-
-        <Users />
-
-      </div>
+      <Router>
+        <div className="container">
+          <Router>
+            <Navbar title="User App" />
+            <hr />
+            <Route exact path="/" component={Users} />
+            <Route exact path="/add" component={AddUser} />
+            
+          </Router>
+        </div>
+      </Router>
     );
   }
 }
